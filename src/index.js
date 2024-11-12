15d99f30e548267a5c61f25e61cdd87aedccb30d
laurenceKohli <laurence.kohli@hes-so.com>
2024-11-12 14:49:08 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// Pour importer les données vous pouvez soit importer directement les csv (@rollup/plugin-dsv), soit utiliser la méthode csv de d3-fetch
+
+// @rollup/plugin-dsv
+// import populationData from "../data/population_total.csv";
+
